0581ace2055d8fb529a67739290eb82ee3d231b4
shangobashi <eloubw@gmail.com>
2025-11-06 19:58:59 +0100

    wrapped listen call so only runs when executed directly

--- api/index.js
+++ api/index.js
@@ -74,9 +74,10 @@ app.get('/search/:searchQuery', async (req, res) => {
 
 
 
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-});
+if (require.main === module) {
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`)
+        });
+}
 
 module.exports = app;
